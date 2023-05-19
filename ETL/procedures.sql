f90710185abf8e980c60f9e89bcc462f0adee1d4
Juan G Garzon <jngzn@hotmail.com>
2023-05-18 23:13:20 -0500

    added grapics and titles

--- ETL/procedures.sql
+++ ETL/procedures.sql
@@ -14,14 +14,27 @@ END
 $$
 DELIMITER $$
 DROP PROCEDURE IF EXISTS listar_anio_conteo$$
-CREATE PROCEDURE lista_anio_conteo()
+CREATE PROCEDURE listar_anio_conteo()
 BEGIN
   select 
-		mp.municipio, year(fecha) as anio, 
+		convert(concat(year(fecha),'.' ,month(fecha)), decimal(5,1)) as anio, 
 		sum(cantAbusados) as conteo 
-	from dbabusos.abuso as ab inner join dbabusos.municipio as mp 
-    on ab.idMunicipio = mp.codDane 
-    group by year(fecha), municipio 
+	from dbabusos.abuso as ab 
+    group by convert(concat(year(fecha),'.' ,month(fecha)), decimal(5,1)) 
     order by sum(cantAbusados) desc;
+END
+$$
+DELIMITER $$
+DROP PROCEDURE IF EXISTS listar_generos_conteo$$
+CREATE PROCEDURE listar_generos_conteo()
+BEGIN
+	SELECT genero, sum(cantAbusados) as cant FROM genero as gn, abuso as ab where gn.idGenero = ab.idGenero group by genero;
+END
+$$
+DELIMITER $$
+DROP PROCEDURE IF EXISTS listar_armas_anio_conteo$$
+CREATE PROCEDURE listar_armas_anio_conteo()
+BEGIN
+	SELECT arma,  year(fecha) as anio, sum(cantAbusados) as cant FROM usoarmas as am, abuso as ab where am.idusoArmas = ab.idArma group by arma, year(fecha);
 END
 $$
